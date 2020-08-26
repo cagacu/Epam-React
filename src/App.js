a0ad7d42daceaa62cd50c6b526a5cae7a85f992b
Çağatay <cagacu@outlook.com>
2020-08-26 10:21:32 +0200

    Task1 code is moved to task1 folder

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import React from "react";
 import "./styles.css";
 import Task1 from "./Tasks/task1.js";
 
-let t1 = (
-  <fieldset>
-    <legend>Task 1</legend>
-    <Task1 />
-  </fieldset>
-);
-
-const container = React.createElement("div", null, t1);
+const container = React.createElement("div", null, Task1);
 
 export default function App() {
   return <div className="App">{container}</div>;
--- src/Tasks/task1.js
+++ src/Tasks/task1.js
@@ -25,13 +25,20 @@ function Header4(props) {
   return <h4> {props.title}</h4>;
 }
 
-let Task1 = () => (
+let T1 = () => (
   <div className="container">
     {Header1}
     <Header2 />
     <Header3 />
     <Header4 title="Hello React !!" />
   </div>
+);
+
+let Task1 = (
+  <fieldset>
+    <legend>Task 1</legend>
+    <T1 />
+  </fieldset>
 );
 
 export default Task1;
