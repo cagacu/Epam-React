dd730afb2922f0d79be042889a1102edf5fb401b
Cagatay Cura <Cagatay_Cura@epam.com>
2020-10-20 12:30:27 +0200

    removes comments

--- src/Components/Movie/Forms/MovieForm.js
+++ src/Components/Movie/Forms/MovieForm.js
@@ -185,60 +185,6 @@ class MovieForm extends React.Component {
           )}
      </Formik>
     );
-
-
-
-
-    // ===========================
-    // const { fetchedMovie } = (this.state.isEditForm) ? this.props : { fetchedMovie : { genres : Array('')} };
-
-    // if(!fetchedMovie && this.state.isEditForm)
-    //   return <>loading</>;
-
-    // let movieIdLabel;
-    // if (this.state.isEditForm) {
-
-    //   movieIdLabel = (
-    //     <Form.Group>
-    //       <Form.Row>
-    //         <Form.Label row>MOVIE ID</Form.Label>
-    //       </Form.Row>
-    //       <Form.Row>
-    //         <Form.Label row>{this.props.movieid}</Form.Label>
-    //       </Form.Row>
-    //     </Form.Group>
-    //   );
-    // } else movieIdLabel = null;
-
-    // return (
-    //   <>
-    //     {movieIdLabel}
-    //     <Form.Group>
-    //       <Form.Label>Title</Form.Label>
-    //       <Form.Control type="text" value={fetchedMovie.title} name="title" />
-    //     </Form.Group>
-    //     <Form.Group>
-    //       <Form.Label>Release Date</Form.Label>
-    //       <Form.Control type="date" value={fetchedMovie.release_date} name="date"/>
-    //     </Form.Group>
-    //     <Form.Group>
-    //       <Form.Label>Movie URL</Form.Label>
-    //       <Form.Control type="url" value={fetchedMovie.poster_path} name="url"/>
-    //     </Form.Group>
-    //     <Form.Group>
-    //       <Form.Label>Genre</Form.Label>
-    //       <MovieGenreSelect selectedValue={fetchedMovie.genres[0]} name="genre"/>
-    //     </Form.Group>
-    //     <Form.Group>
-    //       <Form.Label>Overview</Form.Label>
-    //       <Form.Control type="text" value={fetchedMovie.overview} name="overview"/>
-    //     </Form.Group>
-    //     <Form.Group>
-    //       <Form.Label>Runtime</Form.Label>
-    //       <Form.Control type="number" value={fetchedMovie.runtime} name="runtime"/>
-    //     </Form.Group>
-    //   </>
-    // );
   }
 }
 
